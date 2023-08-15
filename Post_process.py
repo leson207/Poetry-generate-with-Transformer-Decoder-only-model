acc129d704820bc71eb8a23374fff6336843c7ce
leson207 <74070396+leson207@users.noreply.github.com>
2023-08-16 01:20:22 +0700

    Add files via upload

---
+++ Post_process.py
@@ -0,0 +1,106 @@
+import numpy as np
+import tensorflow as tf
+from keras_preprocessing.sequence import pad_sequences
+
+
+class Normal_search(tf.Module):
+    def __init__(self, tokenizer, transformer):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.transformer = transformer
+
+    def __call__(self, sentences, max_length=128, num_gen=20):
+
+        sentences = sentences.numpy()
+        if type(sentences) == np.ndarray:
+            sentences = [s.decode() for s in sentences]
+        else:
+            sentences = sentences.decode()
+
+        # delete above code for input string or list of strings as input ony
+
+        if type(sentences) == str:
+            sentences = [sentences]
+
+        tokens = self.tokenizer.texts_to_sequences(sentences)
+        for i in range(num_gen):
+            inputs = pad_sequences(tokens, maxlen=max_length, padding='pre', truncating='pre')
+
+            predictions = self.transformer(tf.convert_to_tensor(inputs), training=False)
+
+            predictions = predictions[:, -1:, :]  # Shape `(batch_size, 1, vocab_size)`.
+
+            predicted_id = tf.argmax(predictions, axis=-1)
+
+            for j in range(len(sentences)):
+                tokens[j] = tokens[j] + [predicted_id.numpy()[j][0]]
+
+        return self.tokenizer.sequences_to_texts(tokens)
+
+
+class Beam_search(tf.Module):
+    def __init__(self, tokenizer, transformer):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.transformer = transformer
+
+    def __call__(self, sentences, max_length=128, num_gen=20):
+
+        sentences = sentences.numpy()
+        if type(sentences) == np.ndarray:
+            sentences = [s.decode() for s in sentences]
+        else:
+            sentences = sentences.decode()
+
+        if type(sentences) == str:
+            sentences = [sentences]
+
+        band_width = 3
+        tokens = self.tokenizer.texts_to_sequences(sentences)
+        cur_seq = []
+        cur_prob = []
+        cur_len = []
+        batch_size = len(sentences)
+        for j in range(batch_size):
+            cur_seq = cur_seq + [tokens[j]] * band_width
+
+            cur_prob = cur_prob + [0.] * band_width
+
+            cur_len = cur_len + [0] * band_width
+
+        for i in range(num_gen):
+            inputs = pad_sequences(cur_seq, maxlen=max_length, padding='pre', truncating='pre')
+
+            predictions = self.transformer(tf.convert_to_tensor(inputs), training=False)
+
+            predictions = predictions[:, -1:, :]  # Shape `(batch_size*band_width, 1, vocab_size)`.
+
+            _, predicted_id = tf.math.top_k(predictions, k=band_width)
+
+            for j in range(0, batch_size * band_width, band_width):
+                candidate_list = []
+                for k in range(j, j + band_width):
+                    for t in range(band_width):
+                        idx = predicted_id.numpy()[k][0][t]
+                        next_list = cur_seq[k] + [idx]
+                        next_prob = cur_prob[k] + np.log(predictions.numpy()[k][0][idx])
+                        next_len = cur_len[k] + 1
+                        mean_pob = next_prob / next_len
+
+                        candidate_list = candidate_list + [(next_list, next_prob, next_len, mean_pob)]
+
+                sorted_list = sorted(candidate_list, key=lambda x: x[-1], reverse=True)
+                for k in range(j, j + band_width):
+                    cur_seq[k] = candidate_list[k - j][0]
+                    cur_prob[k] = candidate_list[k - j][1]
+                    cur_len[k] = candidate_list[k - j][2]
+
+        gen_text = []
+
+        for i in range(0, batch_size * band_width, band_width):
+            tmp = cur_prob[i:i + band_width]
+            idx = tmp.index(max(tmp))
+            print(cur_len[i + idx])
+            gen_text = gen_text + [cur_seq[i + idx]]
+        # return tokenizer.sequences_to_texts(cur_seq)
+        return self.tokenizer.sequences_to_texts(gen_text)
